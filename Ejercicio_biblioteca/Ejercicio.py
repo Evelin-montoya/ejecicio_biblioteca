287b95e71fa0185092a4c7d0c14869cdd6503622
JeronimoVM <evelinlamejor2004@gmail.com>
2023-09-02 14:52:00 -0500

    Update Ejercicio.py
    Se agrega función registrar libro

--- Ejercicio_biblioteca/Ejercicio.py
+++ Ejercicio_biblioteca/Ejercicio.py
@@ -1,29 +1,20 @@
 ##Primera funcion evelin
 
+# Registro de libro
+libro = []
+
+# Registro de un nuevo usuario
+def registrar_libros ():
+    isbn = input("Ingrese el ibsn: ")
+    nombre = input("Nombre: "),
+    autor = input("Autor: "),
+    num_paginas = input("Número de paginas: ")
+    año_publicacion = input("Año de publicación: ")
+    libro.append({"Ingrese el ibsn": isbn,"Nombre": nombre, "Autor": autor, "Número de paginas": num_paginas, "Año de publicación":año_publicacion})
+    print("Registro exitoso.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(registrar_libros())
 
 
 #funcion kelly
